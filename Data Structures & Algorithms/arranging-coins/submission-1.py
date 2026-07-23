fe788a4b8b235e01504212883b85e12bc769452b
shubhampanchal6376 <shubhampanchal6376@gmail.com>
2026-07-23 22:05:38 +0530

    Add: arranging-coins - submission-1

---
+++ Data Structures & Algorithms/arranging-coins/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        ans = 0
+        i = 1 
+        while n>=i:
+            ans+=1
+            n-=i
+            i+=1
+        return ans
