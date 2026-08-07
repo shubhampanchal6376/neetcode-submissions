1111587402dbf633830c1a57b8edacba9b216315
shubhampanchal6376 <shubhampanchal6376@gmail.com>
2026-08-07 10:15:19 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,9 @@
+from functools import lru_cache
+class Solution:
+    @lru_cache(maxsize = None)
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        return self.climbStairs(n-1)+self.climbStairs(n-2)
